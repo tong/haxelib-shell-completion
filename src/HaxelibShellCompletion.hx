cb7df6ee99915d28c18e1e0872974c49bfff0664
tong <tong@disktree.net>
2013-09-21 04:26:34 +0200

    ...

--- src/HaxelibShellCompletion.hx
+++ src/HaxelibShellCompletion.hx
@@ -4,40 +4,42 @@ import sys.io.File;
 
 using StringTools;
 
-/*
+/**
 	Haxelib shell completion
 */
+@:require(neko)
 class HaxelibShellCompletion {
 
-	public static var haxelib_path(default,null) = Sys.getEnv("HAXELIB");
+	public static var haxelib_path(default,null) = Sys.getEnv( "HAXELIB" );
 	public static var haxelib_commands(default,null) = ["install","list","upgrade","update","selfupdate","remove","set","search","info","user","register","submit","setup","config","path","run","local","dev","git","proxy"];
 
 	public static function haxelib_complete( words : Array<String>, cword : Int ) : Array<String> {
 		if( words.length == 0 )
 			return haxelib_commands;
 		var prev = words[cword-1];
-		switch prev {
-		case "config","proxy","register","search","selfupdate","setup","upgrade","user":
-			return null;
+		return switch prev {
+		case "config","list","proxy","register","search","selfupdate","setup","upgrade","user":
+			null;
 		case "install":
-			return null; //TODO getAvailableLibraries();
-		case "dev","git","info","list","path","set","update","remove":
-			return getInstalledLibraries();
+			null; //getAvailableLibraries();
+		case "dev","git","info","path","set","update","remove":
+			getInstalledLibraries();
 		case "local","submit":
 			var reply = new Array<String>();
 			var path = Sys.getCwd();
 			for( f in FileSystem.readDirectory( path ) )
 				if( f.endsWith('.zip') )
 					reply.push( f );
-			return reply;
+			reply;
 		case "run":
-			return getRunableLibraries();
+			getRunableLibraries();
 		case _:
 			switch words[0] {
 			case "run":
 				var lib = words[1];
 				if( lib != null ) {
 					var libPath = getHaxelibLibraryPath( lib );
+					//TODO check if runable
 					var params = words.slice(2);
 					var index = cword-2;
 					if( libPath != null ) {
@@ -45,9 +47,9 @@ class HaxelibShellCompletion {
 						return r;
 					}
 				}
-				return null;
+				null;
 			case _:
-				return haxelib_commands;
+				haxelib_commands;
 			}
 		}
 	}
@@ -55,10 +57,11 @@ class HaxelibShellCompletion {
 	static function runCompletionModule( mod : String, params : Array<String>, index : Int ) : Array<String> {
 		var l = neko.vm.Loader.local();
 		var m = l.loadModule( mod );
-		var cl = m.exportsTable().__classes;
+		var cl : Dynamic = m.exportsTable().__classes;
 		try return cl.ShellCompletion.complete( params, index ) catch(e:Dynamic) {
 			return null;
 		}
+		return null;
 	}
 
 	static function getInstalledLibraries() : Array<String> {
@@ -101,40 +104,22 @@ class HaxelibShellCompletion {
 		return '$path/$version';
 	}
 
-	static function main() {
-		
-		if( Sys.systemName() != 'Linux' ) {
-			Sys.println( 'Your operating system is not supported yet' );
-			Sys.exit( 1 );
-		}
+	static function error( message : Dynamic ) {
+		Sys.println( message );
+		Sys.exit(1);
+	}
 
-		var cwd = Sys.getCwd();
-		var script = cwd+'res/haxelib-completion.sh';
-		var targetDirectory = '/etc/bash_completion.d';
-		var installPath = '$targetDirectory/haxelib';
+	static function main() {
+		if( Sys.systemName() != 'Linux' ) error( 'OS not supported' );
+		if( haxelib_path == null ) error( 'HAXELIB environment variable not set' );
+		if( !FileSystem.exists( haxelib_path ) ) error( 'Haxelib not found ($haxelib_path)' );
 		var args = Sys.args();
-		var cmd = args.shift();
-		switch cmd {
+		switch args.shift() {
 		case "haxelib":
 			var cword = Std.parseInt( args.pop() )-1;
 			var words = haxelib_complete( args, cword );
 			if( words != null )
 				ShellCompletion.writeWords( words );
-		/*
-		case "install":
-			if( FileSystem.exists( installPath ) )
-				FileSystem.deleteFile( installPath );
-			try File.copy( script, installPath ) catch(e:Dynamic) {
-				Sys.println(e);
-				return;
-			}
-			Sys.println( 'Haxelib shell-completion successfully installed' );
-		case "uninstall":
-			try FileSystem.deleteFile( installPath ) catch(e:Dynamic) {
-				Sys.println(e);
-				Sys.exit(1);
-			}
-		*/
 		//case "cache": 
 		//case "update":
 		//case "custom":
